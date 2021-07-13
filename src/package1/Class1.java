aed11a7d3d4d826b58d09bfca73b6f558d320350
nehatawari <nehatawari3768@gmail.com>
2021-07-13 08:47:31 +0530

    B1

--- src/package1/Class1.java
+++ src/package1/Class1.java
@@ -2,7 +2,8 @@ package package1;
 
 public class Class1 {
 	public static void main(String[] args) {
-		System.out.println("class1");
+		System.out.println("class1 A");
+		System.out.println("class1 B1");
 	}
 
 }
